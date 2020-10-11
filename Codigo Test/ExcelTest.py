973c467824983b44382a0f0e4550da083a3d5506
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2020-10-11 12:43:36 -0500

    Añadi Preguntas al excel
    Falta Hacer cambios en el codigo

--- Codigo Test/ExcelTest.py
+++ Codigo Test/ExcelTest.py
@@ -4,7 +4,7 @@ import pandas as pd #recuerda instalar modulo pandas
 #cambien la ruta dependiendo de donde guarden el excel
 filePath="/Users/sergiogonzalez/Documents/GitHub/ProyectoIntegradorPrimerSemestre/Archivo Exel/PInt.xlsx"
 readFile=pd.read_excel(filePath)
-selectCell=readFile["Nombres"][0] #encontrar el valor especifico de una celda 
+selectCell=readFile["Preguntas"][0] #encontrar el valor especifico de una celda 
 
 selectCell=str(selectCell.lower()) #Transforma el valor de la celda en str y la pasa a lower
 res=input("escribe texto ")
