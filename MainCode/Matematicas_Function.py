b4c342aafd8ef640a3d2f1d68ef11da010116888
Isabelinna <38509593+isabelinna@users.noreply.github.com>
2020-10-18 15:45:40 -0500

    Update Matematicas_Function.py

--- MainCode/Matematicas_Function.py
+++ MainCode/Matematicas_Function.py
@@ -1,11 +1,12 @@
 def Matematicas ():
+   
    import time
     #Función que muestra si aún hay tiempo. restando el tiempo presente del límite
     def tiempot(tlim, time_0):
         tnow = time.perf_counter()
         return (tnow - time_0 > tlim)        
 
-    #Función para darle el formato a las preguntas del test
+    #Función para darle el formato a las preguntas del test, ingresa número pregunta,el string de la pregunta la respuesta y la respuesta correcta
     def pregunta(nump, pregunt, opciones, correcta, cambiot=5)
         #El cambio de tiempo por default es 5, sin embargo se puede cambiar aquí.
         
@@ -16,21 +17,23 @@ def Matematicas ():
         respuesta = int(input("respuesta: "))
         print("")
         #Si la respuesta es igual a la corracta, entonces regresa cambio de tiempo positivo.
-        #Si es incorrecta, el cambio de tiempo es negativo.
+       
         
         if (respuesta==correcta):
             print("¡Respuesta Correcta! +5 segundos.")
             return cambiot
+         
+          #Si es incorrecta, el cambio de tiempo es negativo.
         else:
             print("¡Respuesta incorrecta! -5 segundos.")
             return -cambiot
 
-
+   #Ingresar el tiempo límite y el tiempo presente
     def quiz_mate():
         tlim = int(input("Cuánto tiempo quieres? "))
         print("Límite de tiempo: {}".format(tlim))
         time_0 = time.perf_counter()
-
+      #incremento de tiempo (tinc) define el formato de la pregunta con la funcion pregunta
         tinc = pregunta(1, "7/5 + 2/3 - 1 = ?", 
                                      "1) 17/5   2) 8/8   3) 16/15",3,)
 
@@ -39,7 +42,7 @@ def Matematicas ():
         tinc = pregunta(2, "Fórmula del área de un cuadrado?", 
                                      "1) L*L   2) 2L+L   3) 4*L",1,)
         tlim = tlim + tinc
-
+      #Compara si el tiempo presente es mayor al tiempo límite, si no, entonces ya se acabó
         if (tiempot(tlim, time_0)):
             print('Se acabó el tiempo!')
             return    
@@ -57,11 +60,12 @@ def Matematicas ():
         tlim = tlim + tinc
 
         elapsed=-time.perf_counter()+tlim
-
+      #Se vuelve a comparar el tiempo después de las preguntas
         if (tiempot(tlim, time_0)):
             print('Se acabó el tiempo!')
             return
-
+      #Si el usuario llega al final sin que se le acabe el tiempo, se muestra el tiempo sobrante
+      #Se muestra un mensaje de bien hecho
         print('¡Bien hecho!')
         print("Te sobraron:",round(elapsed,2), "segundos")
     quiz_mate()
